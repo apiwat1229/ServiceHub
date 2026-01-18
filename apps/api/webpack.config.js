67b81b5322f6af0ad7ebc95e66575462683cc163
admin <admin@localhost.localdomain>
2026-01-18 10:09:51 -0500

    fix(api): ignore missing @fastify/static in webpack and update package-lock

--- apps/api/webpack.config.js
+++ apps/api/webpack.config.js
@@ -28,7 +28,7 @@ module.exports = function (options, webpack) {
         plugins: [
             ...options.plugins,
             new webpack.IgnorePlugin({
-                resourceRegExp: /^(mock-aws-s3|aws-sdk|nock)$/,
+                resourceRegExp: /^(mock-aws-s3|aws-sdk|nock|@fastify\/static)$/,
             }),
         ],
     };
